a128fad7ec00b87570c887bc65de7f4a0636bc6c
Ahmet Eren Yaşar <ahmeterenyasar@Ahmet-MacBook-Pro.local>
2025-09-29 12:25:49 +0300

    feat: button playground

--- flutter_00/lib/101/button_learn.dart
+++ flutter_00/lib/101/button_learn.dart
@@ -1,57 +1,60 @@
 import 'package:flutter/material.dart';
 
 class ButtonLearn extends StatelessWidget {
-  ButtonLearn({Key? key}) : super(key: key);
+  const ButtonLearn({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextButton(onPressed: () {}, child: Text('data')),
-          ElevatedButton(onPressed: null, child: Text('data')),
-          IconButton(onPressed: () {}, icon: Icon(Icons.abc_rounded)),
+          Center(),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                textStyle: WidgetStatePropertyAll(TextStyle(fontSize: 25)),
+              ),
+              child: Text('Cancel'),
+            ),
+
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.star),
+            label: Text('Favorite'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+              textStyle: WidgetStatePropertyAll(
+                TextStyle(
+                  fontSize: 25,
+                  foreground: Paint()..color = Colors.black,
+                ),
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.access_alarm),
+            iconSize: 50,
+          ),
           FloatingActionButton(
             onPressed: () {},
-            child: Icon(Icons.account_box),
+            shape: CircleBorder(),
+            child: Icon(Icons.add),
           ),
           OutlinedButton(
+            onPressed: () {},
             style: OutlinedButton.styleFrom(
-              backgroundColor: Colors.red,
-              padding: EdgeInsets.all(10),
-            ),
-            onPressed: () {},
-            child: SizedBox(width: 200, child: Text('data')),
-          ),
-          OutlinedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.abc),
-            label: Text('data'),
-          ),
-          InkWell(onTap: () {}, child: Text('custom')),
-          Container(height: 200, color: Colors.white),
-          SizedBox(height: 10),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
+              side: const BorderSide(color: Colors.red, width: 4),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+                borderRadius: BorderRadius.circular(12),
               ),
             ),
-            onPressed: () {},
-            child: Padding(
-              padding: EdgeInsets.only(
-                top: 20,
-                bottom: 20,
-                right: 40,
-                left: 40,
-              ),
-              child: Text(
-                'Place Bid',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ),
+            child: const Text('Outlined Button'),
           ),
+          // InkWell(),
         ],
       ),
     );
--- flutter_00/lib/main.dart
+++ flutter_00/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_00/101/button_learn.dart';
-import 'package:flutter_00/101/container_sized_box_learn.dart';
 
 void main() {
   runApp(const MyApp());
